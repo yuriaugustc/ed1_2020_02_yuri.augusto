1e28c2a0fcd563cd05616425c6f20c69f0a56376
yuriaugustc <yuri.augusto000@gmail.com>
2021-07-18 10:52:40 -0300

    ex04 feito

--- Lab01 - Expressions/ex04/ex04.c
+++ Lab01 - Expressions/ex04/ex04.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
 int main() {
+    float nota1, nota2, nota3, nota4;
+    int nfinal;
     printf("<< Media Aritmetica >>\n");
+
+    printf("Digite a 1a nota: ");
+    scanf("%f", &nota1);
+    printf("Digite a 2a nota: ");
+    scanf("%f", &nota2);
+    printf("Digite a 3a nota: ");
+    scanf("%f", &nota3);
+    printf("Digite a 4a nota: ");
+    scanf("%f", &nota4);
+    printf("===Notas===\n");
+    printf("Nota 1: %.2f; Nota 2: %.2f; Nota 3: %.2f; Nota 4: %.2f\n", nota1, nota2, nota3, nota4);
+    nfinal = (nota1+nota2+nota3+nota4)/4;
+    printf("Media: %d\n", nfinal);
+
 }
 
 /*
