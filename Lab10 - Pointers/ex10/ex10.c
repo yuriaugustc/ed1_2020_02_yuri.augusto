e4e55dd5e028da62a09edacc57ede8b4df9eac5b
yuriaugustc <yuri.augusto000@gmail.com>
2021-08-15 17:13:10 -0300

    Exercicio Pronto

--- Lab10 - Pointers/ex10/ex10.c
+++ Lab10 - Pointers/ex10/ex10.c
@@ -4,7 +4,7 @@ int main(){
     int vet[] = {1,2,3,4,5,6,7,8,9,10};
     printf("<< Pointers >>\n");
     for(int i = 9; i >= 0; i--){
-        printf("%d,", vet[i]);     
+        printf("%d,", *(vet+i));     
     }
     return 0;
 }
