7bb23a542ca7db9a031c517b01813d9cea2300e7
yuriaugustc <yuri.augusto000@gmail.com>
2021-08-18 23:35:36 -0300

    exercicio pronto

--- Lab11 - Dynamic allocation/ex05/ex05.c
+++ Lab11 - Dynamic allocation/ex05/ex05.c
@@ -1,12 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 //lembre-se de incluir as bibliotecas adequadas
+struct ponto{
+    int x,y;
+};
 
 int main(){
-    printf("<< Vetor de pontos alocados dinamicamente >>");
-
+    int n, esq, dir, cim, baix, aux = 0,aux1 =0, aux2 = 0, aux3 = 0;
+    struct ponto *pointer, *esquerda, *direita, *cima, *baixo;
+    printf("Quantos pontos deseja digitar: ");
+    scanf("%d", &n);
+    pointer = (int *)malloc(n*sizeof(struct ponto));
+    for(int i = 0; i < n; i++){
+        printf("Entre com a coordenada x do ponto %d:", i+1);
+        scanf("%d", &pointer[i].x);
+        printf("Entre com a coordenada y do ponto %d:", i+1);
+        scanf("%d", &pointer[i].y);
+        printf("\n");
+        if((pointer[i].x) < esq){
+            esq = pointer[i].x;
+            esquerda = &pointer[i];
+        }
+        else if((pointer[i].x) > dir){
+            dir = pointer[i].x;
+            direita = &pointer+i; 
+        }
+        else if((pointer[i].y) < baix){
+            baix = pointer[i].y;
+            baixo = &pointer+i; 
+        }
+        else if((pointer[i].y) > cim){
+            cim = pointer[i].y;
+            cima = &pointer+i;
+        }
+    }
+    printf("Pontos digitados: ");
+    for(int i = 0; i < n; i++){
+        printf("(%d,%d); ", pointer[i].x, pointer[i].y);
+    }
+    printf("\nPonto mais a esquerda: (%d,%d);\n", esquerda[0].x,esquerda[0].y);
+    printf("Ponto mais a direita: (%d,%d);\n", direita[0].x,direita[0].y);
+    printf("Pronto mais acima: (%d,%d);\n"), cima[0].x,cima[0].y;
+    printf("Pronto mais abaixo: (%d,%d);\n"), baixo[0].x,baixo[0].y;
     return 0;
 }
-
 /*
 5) Modifique o programa anterior incluindo quatro ponteiros, que apontarão para
 os seguintes pontos: mais a esquerda, mais a direita, mais acima, mais abaixo.
