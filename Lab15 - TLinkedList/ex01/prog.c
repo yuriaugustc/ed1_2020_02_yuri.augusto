ac2ee502401413ebe4011502b362963d7aa2f23f
Yuri Augusto da Costa <yuri.costa@sedoc.local>
2021-09-24 15:26:55 -0300

    Lab15

--- Lab15 - TLinkedList/ex01/prog.c
+++ Lab15 - TLinkedList/ex01/prog.c
@@ -6,32 +6,53 @@
 int main(){
     TLinkedList *alunos_pet;
     alunos_pet = list_create();
-    aluno *aluno1 = malloc(sizeof(aluno));
-    char nome[30];
-    int matricula;
-    float n1, n2, n3;
+    aluno al, al1, al2, al3, al4;
+    aluno aluno1, aluno2, aluno3, aluno4;
 
-    aluno1->matricula = 1;
-    strcpy(aluno1->nome, "Joaozinho");
-    aluno1->n1 = 3.4;
-    aluno1->n2 = 6.9;
-    aluno1->n3 = 9.9;
+    al1.matricula = 1;
+    strcpy(al1.nome, "Joaozinho");
+    al1.n1 = 3.4;
+    al1.n2 = 6.9;
+    al1.n3 = 9.9;
 
-    list_push_front(alunos_pet, *aluno1);
-    list_push_back(alunos_pet, *aluno2);
-    list_insert(alunos_pet, );
-    list_insert_sorted(alunos_pet, );
+    al2.matricula = 2;
+    strcpy(al2.nome, "Josezinho");
+    al2.n1 = 6.1;
+    al2.n2 = 2.6;
+    al2.n3 = 4.2;
+
+    al3.matricula = 3;
+    strcpy(al3.nome, "Juninho");
+    al3.n1 = 8.9;
+    al3.n2 = 7.9;
+    al3.n3 = 9.9;
+
+    al4.matricula = 4;
+    strcpy(al4.nome, "Joaziania");
+    al4.n1 = 8.9;
+    al4.n2 = 7.9;
+    al4.n3 = 9.9;
+
+    list_push_front(alunos_pet, al1);
+    list_push_back(alunos_pet, al2);
+    list_insert(alunos_pet, 3, al3);
+    list_insert_sorted(alunos_pet, al4);
     list_size(alunos_pet);
-    list_pop_front(alunos_pet, );
-    list_pop_back(alunos_pet, );
-    list_erase_data(alunos_pet, );
-    list_erase_pos(alunos_pet, );
-    list_find_pos(alunos_pet, );
-    list_find_mat(alunos_pet, );
-    list_front(alunos_pet, );
-    list_back(alunos_pet, );
-    list_get_pos(alunos_pet, );
+    list_find_pos(alunos_pet, 4, &aluno1);
+    list_find_mat(alunos_pet, 4, &aluno2);
+    list_front(alunos_pet, &aluno3);
+    list_back(alunos_pet, &aluno4);
+    list_get_pos(alunos_pet, 4);
     list_print(alunos_pet);
-
+    list_pop_front(alunos_pet);
+    list_pop_back(alunos_pet);
+    list_erase_data(alunos_pet, 3);
+    list_erase_pos(alunos_pet, 2);
+    list_push_front(alunos_pet, al1);
+    list_push_back(alunos_pet, al2);
+    list_insert(alunos_pet, 3, al3);
+    list_insert_sorted(alunos_pet, al4);
+    list_free(alunos_pet);
+    
     return 0;
 }
